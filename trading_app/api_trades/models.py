ccc614708206f64acfe0f1fb45f6b056628df80a
bob134552 <bob134552@hotmail.com>
2024-08-08 01:53:16 +0000

    Initial Commit
    -Initial settings configured
    -Added set up for dev in gitpod
    -Created api_trades app
    -Created Order and Stock model classes

---
+++ trading_app/api_trades/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Stock(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    ORDER_CHOICES = [
+        ('buy', 'Buy'),
+        ('sell', 'Sell'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    order_type = models.CharField(max_length=4, choices=ORDER_CHOICES, null=False, blank=False)
+    quantity = models.PositiveIntegerField(null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.stock.name} - {self.order_type} - {self.quantity}"
